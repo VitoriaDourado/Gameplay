16b8be1657b97051567f43e964333324c70f836f
Vitoria Dourado <vickawaii14@gmail.com>
2023-07-28 11:08:50 -0300

    :sparkles: :feat: Adding scripts

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -5,11 +5,7 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float lowerBound = -10;
 
     // Update is called once per frame
     void Update()
@@ -17,6 +13,11 @@ public class DestroyOutOfBounds : MonoBehaviour
         if(transform.position.z > topBound)
         {
             Destroy(gameObject); 
+        }
+        else if(transform.position.z < lowerBound)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
         }
     }
 }
